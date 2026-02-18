d3c28bee70bbf1589b17dd93edde06773a67049d
Maurya Suryakant <mauryasuryakant99@gmail.com>
2026-02-18 12:56:49 +0530

    javaaaaa

---
+++ Java/final_9a.java
@@ -0,0 +1,19 @@
+class A {
+	final int a = 10;
+	void display() {
+		System.out.print("I'm Class A");
+	}
+}
+
+class B {
+	int a = 10;
+	final void display() {
+		System.out.print("I'm Class A");
+	}
+}
+
+class P9_1 {
+	static void main (String a[]) {
+	
+	}
+}
